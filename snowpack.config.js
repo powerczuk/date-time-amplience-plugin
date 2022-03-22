56224439432aaa2208a2220addc56fc6e84124dd
Pawel Owerczuk <pawel.owerczuk@intentive.de>
2022-03-22 02:07:03 +0100

    Css updates

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: '/', static: true },
+    public: { url: '/', static: true, resolve: false },
     src: { url: '/dist' },
   },
   plugins: ['@snowpack/plugin-svelte', '@snowpack/plugin-dotenv'],
